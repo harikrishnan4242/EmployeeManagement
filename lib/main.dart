7fd7aca4dee10c0b5e81ccfbf9ebc0071bf2ad8a
harikrishnan4242 <hari@thoughtfulviewfinder.in>
2024-05-26 09:00:28 +0530

    task-2

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Employee Management',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black,
         elevatedButtonTheme: ElevatedButtonThemeData(
